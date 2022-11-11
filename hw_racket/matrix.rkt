32c9a17e8c6707971e5b0be2363763374919aa85
Aneta Potomská <potomane@fit.cvut.cz>
2022-11-11 15:12:15 +0100

    hw_racket: matrix fix transpose func

--- hw_racket/matrix.rkt
+++ hw_racket/matrix.rkt
@@ -6,27 +6,18 @@
 ;Můžete se spolehnout, že matice budou vždy mít korektní rozměry a vždy budou
 ;mít alespoň jeden řádek a sloupec.
 
-;volající funkce
-(define (matrix-mul a b)
-  (matrix-mul-final a b '()))
-;finální
-(define (matrix-mul-final a b res)
-  (let ([t (my-transpose b)])
-  (cond [(null? a) (my-transpose res)]
-         [else (matrix-mul-final (cdr a) b (cons (my-mul (car a) t '()) res))])))
 ;udělej transpozici matice
 (define (my-transpose a)
-  (my-tr-final a (my-len (car a)) 0 '())
-  )
+  (my-tr-final a (my-len (car a)) '()))
 ;délka listu
 (define (my-len a)
   (if (null? a)
-      0
+      -1
       (+ 1 (my-len (cdr a)))))
 ;finální pro transpozici
-(define (my-tr-final a len pos res)
-  (cond [(equal? len pos) (my-transpose res)]
-        [else (my-tr-final a len (+ pos 1) (cons (my-col a pos) res))]))
+(define (my-tr-final a len res)
+  (cond [(equal? len -1) res]
+        [else (my-tr-final a (- len 1) (cons (my-col a len) res))]))
 ;n-tý prvek
 (define (my-nth a pos)
   (cond [(null? a) null]
@@ -35,32 +26,20 @@
 ;n-tý sloupec
 (define (my-col a pos)
   (map (lambda (row) (my-nth row pos)) a))
+
+;volající funkce
+(define (matrix-mul a b)
+  (foldl cons null (matrix-mul-final a (my-transpose b) '())))
+;finální
+(define (matrix-mul-final a b res)
+  (cond [(null? a) res] 
+         [else (matrix-mul-final (cdr a) b (cons (my-mul (car a) b '()) res))]))
 ;vytvoř seznam pro násobení první řádky a se všemi z t
 (define (my-mul a b res)
-  (cond [(null? b) res]
+  (cond [(null? b) (foldl cons null res)]
          [else (my-mul a (cdr b) (cons (my-sum a (car b) 0) res))]))
 ;vynásob prvky na stejných pozicích a sečti
 (define (my-sum a b res)
   (cond [(null? a) res]
         [(null? b) res]
          [else (my-sum (cdr a) (cdr b) (+ (* (car a) (car b)) res))]))
-
-    
-;(1 2 3)
-;(4 1 1)
-
-;*
-
-;(1 2)     transpozice
-;(3 4) ->  (1 3 2)
-;(2 1)     (2 4 1)
-
-;=
-
-;(1*1+2*3+3*2 1*2+2*4+3*1)
-;(4*1+1*3+1*2 4*2+1*4+1*1)
-
-;=
-
-;(13 13)
-;(9 13)
